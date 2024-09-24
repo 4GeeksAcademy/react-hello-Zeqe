6d2337907fedfaf88f624daf4438beea2ef13c91
ZeqeB <85196913+ezebellino@users.noreply.github.com>
2024-09-24 02:24:21 +0000

    realizando cambios

--- src/js/component/App.js
+++ src/js/component/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import Navbar from './Navbar.js'; 
-import Jumbotron from './Jumbotron.js'; 
-import CardsList from './Card.js'; 
-import Footer from './Footer.js';
+import Navbar from './Navbar'; 
+import Jumbotron from './Jumbotron'; 
+import Card from './Card'; 
+import Footer from './Footer';
 
 function App() {
+  const cardsData = [
+    { title: "Card Title 1", text: "This is a description.", imgSrc: "https://via.placeholder.com/500x325" },
+    { title: "Card Title 2", text: "This is a description.", imgSrc: "https://via.placeholder.com/500x325" },
+    { title: "Card Title 3", text: "This is a description.", imgSrc: "https://via.placeholder.com/500x325" },
+    { title: "Card Title 4", text: "This is a description.", imgSrc: "https://via.placeholder.com/500x325" }
+  ];
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <div className="container mt-5">
+      <div className="container-fluid px-4 mt-5">
         <Jumbotron />
-        <CardsList />
+        <div className="row">
+          {cardsData.map((card, index) => (
+            <Card 
+              key={index}
+              title={card.title}
+              text={card.text}
+              imgSrc={card.imgSrc}
+            />
+          ))}
+        </div>
       </div>
       <Footer />
     </div>
--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -15,21 +15,5 @@ function Card({ title, text, imgSrc }) {
   );
 }
 
-function CardsList() {
-  const cardsData = [
-    { title: "Card Title", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae nulla aliquid animi culpa aspernatur magni numquam a fugit neque! Animi distinctio deserunt explicabo eaque perspiciatis, recusandae maxime ex tempora doloremque." , imgSrc: "https://via.placeholder.com/500x325" },
-    { title: "Card Title", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae nulla aliquid animi culpa aspernatur magni numquam a fugit neque! Animi distinctio deserunt explicabo eaque perspiciatis, recusandae maxime ex tempora doloremque." , imgSrc: "https://via.placeholder.com/500x325" },
-    { title: "Card Title", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae nulla aliquid animi culpa aspernatur magni numquam a fugit neque! Animi distinctio deserunt explicabo eaque perspiciatis, recusandae maxime ex tempora doloremque." , imgSrc: "https://via.placeholder.com/500x325" },
-    { title: "Card Title", text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae nulla aliquid animi culpa aspernatur magni numquam a fugit neque! Animi distinctio deserunt explicabo eaque perspiciatis, recusandae maxime ex tempora doloremque." , imgSrc: "https://via.placeholder.com/500x325" }
-  ];
+export default Card;
 
-  return (
-    <div className="row">
-      {cardsData.map((card, index) => (
-        <Card key={index} title={card.title} text={card.text} imgSrc={card.imgSrc} />
-      ))}
-    </div>
-  );
-}
-
-export default CardsList;
